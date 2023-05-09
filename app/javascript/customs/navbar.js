29958f017f23fa5a3fa0c77c5e4864832eefb867
MauricioNC <mauricionc19@gmailc.om>
2023-05-08 21:47:42 -0600

    Navbar styles
    Was completed the navbar styles for mobile version.
    
    Also, was created a mixinis file, variables file, that will be used in
    other scss files; and a base.scss files to set some base and global
    styles.

---
+++ app/javascript/customs/navbar.js
@@ -0,0 +1,52 @@
+document.addEventListener('turbolinks:load', () => {
+  const hamburguerIcon = document.querySelector(".nav__hamburguer");
+  const closeIcon = document.querySelector(".nav__close");
+  const searchForm = document.querySelector(".search__bar");
+  const showFormIcon = document.querySelector(".fa-magnifying-glass");
+  const hideFormIcon = document.querySelector(".fa-arrow-right");
+
+  hamburguerIconEvent("click", hamburguerIcon, closeIcon);
+  closeIconEvent("click", closeIcon, hamburguerIcon);
+  showSearchForm("click", searchForm, showFormIcon);
+  hideSearchForm("click", searchForm, hideFormIcon);
+});
+
+function hamburguerIconEvent(event, hamburguerIcon, closeIcon)
+{
+  const navLinks = document.querySelector(".nav__links");
+
+  hamburguerIcon.addEventListener(event, (e) => {
+    e.preventDefault();
+
+    hamburguerIcon.style.transform = "scale(0)";
+    closeIcon.style.transform = "scale(1)";
+    navLinks.style.transform = "translate(0%)";
+  });
+}
+
+function closeIconEvent(event, hamburguerIcon, closeIcon)
+{
+  const navLinks = document.querySelector(".nav__links");
+
+  hamburguerIcon.addEventListener(event, (e) => {
+    e.preventDefault();
+    hamburguerIcon.style.transform = "scale(0)";
+    closeIcon.style.transform = "scale(1)";
+    navLinks.style.transform = "translate(-100%)";
+  });
+}
+
+function showSearchForm(event, searchForm, showFormIcon)
+{
+  showFormIcon.addEventListener(event, () => {
+    searchForm.style.transform = "translate(0%)";
+  });
+}
+
+function hideSearchForm(event, searchForm, hideFormIcon)
+{
+  hideFormIcon.addEventListener(event, () => {
+    searchForm.style.transform = "translate(100%)";
+  });
+}
+
