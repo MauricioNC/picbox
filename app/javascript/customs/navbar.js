555269c341dfe105d287afedf3f81e532ef3b161
MauricioNC <mauricionc19@gmailc.om>
2023-05-10 16:29:12 -0600

    feat: Navbar styles for laptop and desktop completed.

--- app/javascript/customs/navbar.js
+++ app/javascript/customs/navbar.js
@@ -4,11 +4,9 @@ document.addEventListener('turbolinks:load', () => {
   const searchForm = document.querySelector(".search__bar");
   const showFormIcon = document.querySelector(".fa-magnifying-glass");
   const hideFormIcon = document.querySelector(".fa-arrow-right");
-  const avatar = document.querySelector(".user-icon");
 
   hamburguerIconEvent("click", hamburguerIcon, closeIcon);
   closeIconEvent("click", closeIcon, hamburguerIcon);
-  showUserDropDwonMenu(avatar);
 
   if(window.screen.width <= 320)
   {
@@ -56,14 +54,3 @@ function hideSearchForm(event, searchForm, hideFormIcon)
     searchForm.style.transform = "translate(100%)";
   });
 }
-
-function showUserDropDwonMenu(avatar)
-{
-  const usrDropDwon = document.querySelector(".dropdown__links");
-
-  avatar.addEventListener("click", (e) => {
-    e.preventDefault();
-    usrDropDwon.style.display = "block";
-  });
-}
-
